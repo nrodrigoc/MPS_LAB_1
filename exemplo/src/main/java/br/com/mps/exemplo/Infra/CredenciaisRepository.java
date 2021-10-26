5db56068ee69051c1e1a3321d17c82f123daa3d6
Nathan Jesus <nathan.jesus@conductor.com.br>
2021-10-26 13:15:55 -0300

    divisão em camadas

--- exemplo/src/main/java/br/com/mps/exemplo/Infra/CredenciaisRepository.java
+++ exemplo/src/main/java/br/com/mps/exemplo/Infra/CredenciaisRepository.java
@@ -1,6 +1,6 @@
 package br.com.mps.exemplo.Infra;
 
-import br.com.mps.exemplo.model.Credenciais;
+import br.com.mps.exemplo.business.model.Credenciais;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CredenciaisRepository extends JpaRepository<Credenciais, Long> {
